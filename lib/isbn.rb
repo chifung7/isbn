2780e8bc656cb76abeb3c55e668e9537502d3197
Chi-Fung Fan <chifung@gmail.com>
2012-06-01 19:39:53 +0800

    * re-factored ten, thirteen, as_used, as_new by adding common ISBN::validate
    * raise error for invalid non-digits or non-isbn format
    
      e.g. "B000VH3XBA" (Amazon ASIN)
    
    * use \A instead of ^ in regular expression
    
      e.g. "978\n978571389"
    
    * don't ignore but raise error for invalid checksum in input ISBNs
    
      e.g. "9780820472677" (correct "9780820472676")
    
    * removed Invalid13DigitISBN and Invalid10DigitISBN. used InvalidISBNError instead
    
    * speed up computation of checksum code

--- lib/isbn.rb
+++ lib/isbn.rb
@@ -2,76 +2,83 @@ module ISBN
   extend self
   
   VERSION = "2.0.7"
-  
-  def ten(isbn)
-    raise InvalidISBNError unless isbn.is_a? String
+
+  def validate(isbn)
+    raise InvalidISBNError.new isbn unless isbn.is_a? String
     isbn = isbn.delete("-")
-    raise No10DigitISBNAvailable if isbn =~ /^979/
+
     case isbn.size
-    when 10 then isbn = isbn[0..8]
-    when 13 then isbn = isbn[/(?:^978|^290)*(.{9})\w/,1]
-    else raise Invalid10DigitISBN
+    when 10 then
+      if isbn =~ /\A(\d{9})([\dxX])\z/
+        body, cksum = $1, $2
+        if is_valid_isbn10?(isbn)
+          return [isbn.upcase, nil, body, cksum.upcase]
+        end
+      end
+      raise InvalidISBNError.new isbn
+    when 13 then
+      if isbn =~ /\A(978|979|290|291)(\d{9})(\d)\z/
+        prefix, body, cksum = $1, $2, $3
+        return [isbn, prefix, body, cksum] if is_valid_isbn13?(isbn)
+      end
+      raise InvalidISBNError.new isbn
+    else 
+      raise InvalidISBNError.new isbn
     end
-    case ck = (11 - (isbn.split(//).zip((2..10).to_a.reverse).inject(0) {|s,n| s += n[0].to_i * n[1]} % 11))
-    when 10 then isbn << "X"
-    when 11 then isbn << "0"
-    else isbn << ck.to_s
-    end
+  end
+
+  def ten(isbn)
+    isbn, prefix, body, cksum = validate(isbn)
+    return isbn if prefix.nil? # already an isbn10
+    raise No10DigitISBNAvailable.new isbn if prefix == '979' or prefix == '291'
+
+    cksum10 = isbn10_checksum(body)
+    body << (cksum10 == 10 ? 'X' : cksum10.to_s)
+    return body
   end
   alias :as_ten :ten
   
   def thirteen(isbn)
-    raise InvalidISBNError unless isbn.is_a? String
-    raise Invalid13DigitISBN unless (isbn.size == 10 || isbn.size == 13)
-    isbn = isbn.delete("-")
-    isbn = isbn.rjust(13,"978")[/(.+)\w/,1] # adjust to 13 digit isbn and remove check digit
-    raise Invalid13DigitISBN unless isbn.size == 12 # after adjustments isbn should be 12 digits
-    case ck = (10 - (isbn.split(//).zip([1,3]*6).inject(0) {|s,n| s += n[0].to_i * n[1]} % 10))
-    when 10 then isbn << "0"
-    else isbn << ck.to_s
-    end
+    isbn, prefix, body, cksum = validate(isbn)
+    return isbn if prefix
+
+    isbn12 = '978' + body
+    return isbn12 << isbn13_checksum(isbn12).to_s
   end
   alias :as_thirteen :thirteen
   
   def as_used(isbn)
-    case isbn.size
-    when 13
-      case isbn
-      when /^978/ then thirteen("290#{isbn[3..-1]}")
-      when /^290/ then isbn
-      when /^979/ then thirteen("291#{isbn[3..-1]}")
-      when /^291/ then isbn
-      end
-    when 10 then thirteen("290#{isbn}")
-    else raise ISBN::InvalidISBNError
-    end
+    isbn, prefix, body, cksum = validate(isbn)
+    return isbn if prefix == '290' or prefix == '291'
+
+    isbn12 = case prefix
+             when '978' then '290' + body
+             when '979' then '291' + body
+             when nil then '290' + body
+             else raise ISBN::InvalidISBNError.new isbn
+             end
+    return isbn12 << isbn13_checksum(isbn12).to_s
   end
   alias :used :as_used
 
   def as_new(isbn)
-    case isbn.size
-    when 13
-      case isbn
-      when /^978/ then isbn
-      when /^290/ then thirteen("978#{isbn[3..-1]}")
-      when /^979/ then isbn
-      when /^291/ then thirteen("979#{isbn[3..-1]}")
-      end
-    when 10 then ten(isbn)
-    else raise ISBN::InvalidISBNError
-    end
+    isbn, prefix, body, cksum = validate(isbn)
+    return isbn if prefix.nil? or prefix == '978' or prefix == '979'
+    isbn12 = case prefix
+             when '290' then '978' + body
+             when '291' then '979' + body
+             else raise ISBN::InvalidISBNError.new isbn
+             end
+    return isbn12 << isbn13_checksum(isbn12).to_s
   end
   
   alias :unused :as_new
 
   def valid?(isbn)
-    return false unless isbn.is_a?(String)
-    isbn = isbn.delete("-")
-    case isbn.size
-    when 13 then isbn[-1] == thirteen(isbn)[-1]
-    when 10 then isbn[-1] == ten(isbn)[-1]
-    else false
-    end
+    validate(isbn)
+    true
+  rescue
+    false
   end
   
   def from_image(url)
@@ -89,10 +96,50 @@ module ISBN
     raise InvalidSourceString unless match
     match.to_a.first
   end
-  
+
   class InvalidISBNError < RuntimeError; end
   class No10DigitISBNAvailable < RuntimeError; end
-  class Invalid10DigitISBN < RuntimeError; end
-  class Invalid13DigitISBN < RuntimeError; end
   class InvalidSourceString < RuntimeError; end
+
+  private
+  # http://en.wikipedia.org/wiki/International_Standard_Book_Number
+  def is_valid_isbn13?(isbn13)
+    sum = 0
+    13.times { |i| sum += i.modulo(2)==0 ? isbn13[i].to_i : isbn13[i].to_i*3 }
+    0 == sum.modulo(10)
+  end
+  
+  def isbn13_checksum(isbn12)
+    sum = 0
+    12.times { |i| sum += i.modulo(2)==0 ? isbn12[i].to_i : isbn12[i].to_i*3 }
+    rem = sum.modulo(10)
+    return rem == 0 ? 0 : 10 - rem
+  end
+
+  def is_valid_isbn10?(isbn10)
+    a, b = 0, 0
+    9.times do |i| 
+        a += isbn10[i].to_i # Assumed already converted from ASCII to 0..9
+        b += a
+    end
+    if isbn10[9] == 'x' or isbn10[9] == 'X'
+      a += 10.to_i
+    else
+      a += isbn10[9].to_i
+    end
+    b += a
+
+    return b.modulo(11) == 0
+  end
+
+  def isbn10_checksum(isbn9)
+    a, b = 0, 0
+    9.times do |i| 
+        a += isbn9[i].to_i # Assumed already converted from ASCII to 0..9
+        b += a
+    end
+    rem = (b + a).modulo(11)
+    return rem == 0 ? 0 : 11 - rem
+  end
+
 end
--- test/isbn_spec.rb
+++ test/isbn_spec.rb
@@ -4,34 +4,87 @@ require_relative "../lib/isbn"
 MiniTest::Unit.autorun
 
 describe ISBN do
-  ISBNS = [ ["0820472670","9780820472676"], ["0763740381","9780763740382"], ["0547168292","9780547168296"],
+  ISBNS_DATA = [ ["0820472670","9780820472676"], ["0763740381","9780763740382"], ["0547168292","9780547168296"],
             ["0415990793","9780415990790"], ["1596670274","9781596670273"], ["0618800565","9780618800568"],
             ["0812971256","9780812971255"], ["0465032117","9780465032112"], ["0721606318","9780721606316"],
             ["0887273939","9780887273933"], ["012781910X","9780127819105"], ["0736061819","9780736061810"],
             ["0763748951","9780763748951"], ["0470196181","9780470196182"], ["0736064036","9780736064033"],
             ["0743488040","9780743488044"], ["0470130733","9780470130735"], ["0816516502","9780816516506"],
             ["074324382X","9780743243827"], ["0887401392","9780887401398"], ["0582404800","9780582404809"],
-            ["2906571385","9782906571389"]]
-  
-  it "should respond with a ten digit isbn" do
-    ISBNS.each do |isbn|
-      ISBN.ten(isbn[1]).must_equal isbn[0]
-      ISBN.ten(isbn[0]).must_equal isbn[0]
+            ["2906571385","9782906571389"], ["074324382x","9780743243827"], ["074324382-X","978-074324382-7"] ]
+  ISBNS = ISBNS_DATA.map { |data| data + data.map { |isbn| isbn.delete('-').upcase } }
+
+  describe '#validate' do
+    it "should validate valid isbns" do
+      ISBNS.each do |a|
+        isbn10_orig, isbn13_orig, isbn10, isbn13 = a
+        ISBN.validate(isbn10_orig).must_equal [isbn10, nil, isbn10[0..8], isbn10[9]]
+        ISBN.validate(isbn13_orig).must_equal [isbn13, isbn13[0..2], isbn13[3..11], isbn13[12]]
+      end
     end
-    proc { ISBN.ten("9790879392788") }.must_raise ISBN::No10DigitISBNAvailable
-    proc { ISBN.ten("074324382") }.must_raise ISBN::Invalid10DigitISBN
-    proc { ISBN.ten("") }.must_raise ISBN::Invalid10DigitISBN
-    proc { ISBN.ten(nil) }.must_raise ISBN::InvalidISBNError
+
+    it "should rejects invalid isbns" do
+      proc { ISBN.validate("074324382") }.must_raise ISBN::InvalidISBNError
+      proc { ISBN.validate("") }.must_raise ISBN::InvalidISBNError
+      proc { ISBN.validate(nil) }.must_raise ISBN::InvalidISBNError
+      # invalid non-digits characters
+      proc { ISBN.validate("978074324382A") }.must_raise ISBN::InvalidISBNError
+      # use \A instead of ^ in regular expressions
+      proc { ISBN.validate("978\n978571389") }.must_raise ISBN::InvalidISBNError
+      # incorrect checksum
+      proc { ISBN.validate("0820472671") }.must_raise ISBN::InvalidISBNError
+      proc { ISBN.validate("9780820472677") }.must_raise ISBN::InvalidISBNError
+
+      # invalid characters e.g. Amazon ASIN
+      proc { ISBN.validate("B000VH3XBA") }.must_raise ISBN::InvalidISBNError
+      # incorrect checksum
+      proc { ISBN.validate("0820472671") }.must_raise ISBN::InvalidISBNError
+      proc { ISBN.validate("9780820472677") }.must_raise ISBN::InvalidISBNError
+
+    end
+  end
+  
+  describe '#ten' do
+    it "should respond with a ten digit isbn" do
+      ISBNS.each do |isbn|
+        ISBN.ten(isbn[1]).must_equal isbn[2]
+        ISBN.ten(isbn[0]).must_equal isbn[2]
+      end
+    end
+
+    it "should rejects invalid isbn inputs" do
+      proc { ISBN.ten("9790879392788") }.must_raise ISBN::No10DigitISBNAvailable
+      proc { ISBN.ten("074324382") }.must_raise ISBN::InvalidISBNError
+      proc { ISBN.ten("") }.must_raise ISBN::InvalidISBNError
+      proc { ISBN.ten(nil) }.must_raise ISBN::InvalidISBNError
+      # invalid non-digits characters
+      proc { ISBN.ten("978074324382A") }.must_raise ISBN::InvalidISBNError
+      # use \A instead of ^ in regular expressions
+      proc { ISBN.ten("978\n978571389") }.must_raise ISBN::InvalidISBNError
+      # incorrect checksum
+      proc { ISBN.ten("0820472671") }.must_raise ISBN::InvalidISBNError
+      proc { ISBN.ten("9780820472677") }.must_raise ISBN::InvalidISBNError
+    end
   end
 
-  it "should respond with a thirteen digit isbn" do
-    ISBNS.each do |isbn|
-      ISBN.thirteen(isbn[0]).must_equal isbn[1]
-      ISBN.thirteen(isbn[1]).must_equal isbn[1]
+  describe '#thirteen' do
+    it "should respond with a thirteen digit isbn" do
+      ISBNS.each do |isbn|
+        ISBN.thirteen(isbn[0]).must_equal isbn[3]
+        ISBN.thirteen(isbn[1]).must_equal isbn[3]
+      end
     end
-    proc { ISBN.thirteen("97908793927888") }.must_raise ISBN::Invalid13DigitISBN
-    proc { ISBN.thirteen(nil) }.must_raise ISBN::InvalidISBNError
-    proc { ISBN.thirteen("") }.must_raise ISBN::Invalid13DigitISBN
+
+    it "should rejects invalid isbns" do
+      proc { ISBN.thirteen("97908793927888") }.must_raise ISBN::InvalidISBNError
+      proc { ISBN.thirteen(nil) }.must_raise ISBN::InvalidISBNError
+      proc { ISBN.thirteen("") }.must_raise ISBN::InvalidISBNError
+      # invalid characters e.g. Amazon ASIN
+      proc { ISBN.thirteen("B000VH3XBA") }.must_raise ISBN::InvalidISBNError
+      # incorrect checksum
+      proc { ISBN.thirteen("0820472671") }.must_raise ISBN::InvalidISBNError
+      proc { ISBN.thirteen("9780820472677") }.must_raise ISBN::InvalidISBNError
+    end
   end
   
   it "should convert a NEW isbn into USED" do
@@ -60,9 +113,13 @@ describe ISBN do
     ISBN.valid?("9887401392").must_equal false
     ISBN.valid?("082047267").must_equal false
     ISBN.valid?(nil).must_equal false
+
+    ISBN.valid?("978074324382A").must_equal false
+    ISBN.valid?("978\n978571389").must_equal false
   end
   
   it "should get isbn from source string" do
     ISBN.from_string("ISBN:978-83-7659-303-6\nmore of content").must_equal "978-83-7659-303-6"
   end
 end
+
